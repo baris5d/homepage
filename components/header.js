292a02216a7daa960bf215722cf75a1c6f3bb46e
Barış <dumanist00@gmail.com>
2020-08-08 21:36:12 +0300

    header module

---
+++ components/header.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import styles from './header.module.scss'
+const menu = [
+    { name : 'projects', uri : '/projects' },
+    { name : 'articles', uri : '/articles' }
+]
+export function Header() {
+  return (
+      <header className={styles.header}>
+          <div className={styles.logo}></div>
+          <nav>
+            {menu.map(m => {
+                return(
+                    <Link
+                        key={m.uri}
+                        href={m.uri}
+                    >
+                        <a>{m.name}</a>
+                    </Link>
+                )
+            })}
+          </nav>
+      </header>
+  )
+}
