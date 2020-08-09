b44b5b5672846aee67a72e1aa6039be4a0189514
Barış <dumanist00@gmail.com>
2020-08-09 18:38:21 +0300

    packages

--- components/hire.js
+++ components/hire.js
@@ -2,8 +2,7 @@ import styles from './hire.module.scss'
 
 export function Hire({ text, url, urlText }){
     return(
-        <p>{text}
-            <a href={url} className={styles.link}>{urlText}</a>
+        <p>{text} <a href={url} className={styles.link}>{urlText}</a>
         </p>
     )
 }
