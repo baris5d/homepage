464a84557c3a644bdc31b068f14db5b58bece732
Barış <dumanist00@gmail.com>
2020-08-09 18:15:44 +0300

    hire me message

---
+++ components/hire.js
@@ -0,0 +1,9 @@
+import styles from './hire.module.scss'
+
+export function Hire({ text, url, urlText }){
+    return(
+        <p>{text}
+            <a href={url} className={styles.link}>{urlText}</a>
+        </p>
+    )
+}
