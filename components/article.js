50c535bf060dfee4d4857fdd1a3deae8c6d394f7
Barış <dumanist00@gmail.com>
2020-08-15 23:05:26 +0300

    Font sizes and types

--- components/article.js
+++ components/article.js
@@ -3,10 +3,10 @@ import styles from './article.module.scss'
 export function Article({title, image, description, date, readMin, platform, url}){
     return(
         <section className={styles.article}>
-            <img src={image} alt=""/>
+            <a target="_blank" href={url}><img src={image} alt=""/></a>
             <div className={styles.content}>
                 <h3><a target="_blank" href={url}>{title}</a></h3>
-                <p>{description}</p>
+                <a target="_blank" href={url}><p>{description}</p></a>
                 <span>{date} – {readMin} min. read</span> • <span>on {platform}</span>
             </div>
         </section>
---
+++ context/project/depomarketim.md
@@ -0,0 +1,8 @@
+---
+title: 'Depomarketim'
+description: <p>asdasdasdsad</p>
+address: disleksidernegi.com
+url: https://disleksidernegi.com
+date: 2017-11-09
+image: /images/projects/disleksidernegi.png
+---
