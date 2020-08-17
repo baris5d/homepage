77f65e0afabd9b65a713c219b960e1eb3d9a94f4
Barış <dumanist00@gmail.com>
2020-08-17 13:38:03 +0300

    Inactive control
    Inactive variable in md file is unnecessary I controlled if it has an url or not

--- components/project.js
+++ components/project.js
@@ -2,7 +2,7 @@ import styles from './project.module.scss'
 import IosImage from 'react-ionicons/lib/IosImage'
 import IosLinkOutline from 'react-ionicons/lib/IosLinkOutline'
 
-export function Project({title, image, description, url=false, address, attachmentName, attachmentUrl, demo = false, inactive = false}){
+export function Project({title, image, description, url=false, address, attachmentName, attachmentUrl, demo = false}){
     return(
         <div className={styles.project}>
             <div className={styles.image}>
@@ -14,7 +14,7 @@ export function Project({title, image, description, url=false, address, attachme
                 {url ? <p><a href={url} className={styles.Link}><IosLinkOutline color="#fff"/><span>{address}</span></a></p> : <></>}
                 {attachmentName ? <p><a href={attachmentUrl} className={styles.Link}><IosImage color="#fff" /> <span>{ attachmentName }</span></a></p> : <></>}
                 {demo ? <p><a href={demo} className={styles.Link}><IosLinkOutline color="#fff"/><span>{demo}</span></a></p> : <></>}
-                {inactive ? <p className={styles.inactive}>Inactive</p> : <></>}
+                {!url ? <p className={styles.inactive}>Inactive</p> : <></>}
             </div>
         </div>
     )
