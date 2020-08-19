e3325b9052ad8e914268c5733ee73ca96e247bcb
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-08-19 12:05:53 +0300

    recaptcha

--- components/hire.js
+++ components/hire.js
@@ -1,8 +1,36 @@
+import Recaptcha from 'react-google-invisible-recaptcha';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './hire.module.scss'
 
 export function Hire({ text, url, urlText }){
-    return(
-        <p>{text} <a href={url} className={styles.link}>{urlText}</a>
-        </p>
-    )
+
+  const [flag, setFlag] = useState(1);
+  const recaptcha = useRef(0)
+
+  const onResolved = () => {
+    recaptcha.current.getResponse();
+    setFlag(0);
+  }
+
+  useEffect(() =>{
+    console.log("Recaptcha2")
+    console.log(recaptcha)
+    recaptcha.current.reset();
+    recaptcha.current.execute();
+  },[])
+
+  return(
+      <div>
+        {console.log("Recaptcha1")}
+        {console.log(recaptcha)}
+        <Recaptcha
+          className= "grecaptchaBadge"
+          ref={ ref => recaptcha.current = ref }
+          sitekey= "your-site-key"
+          onResolved={ onResolved }
+        />
+
+        <p>{text} <a href={url} className={styles.link}>{flag ? "" : urlText}</a></p>
+      </div>
+  )
 }
